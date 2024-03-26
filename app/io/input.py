3e0ac80c7210fff131db85321216fc05cc659af1
Danylo Litvinchuk <ltvnchkd@gmail.com>
2024-03-26 02:07:27 +0200

    declared io package funcs

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def read_user_input():
+    return
+
+def read_file_default(path):
+    return
+
+def read_file_pandas(path):
+    return
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def output_to_console(text):
+    return
+
+def output_to_file(text, to_path):
+    return
